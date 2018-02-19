7d89c742d00cff84b2aa448ab90988ebeb1fab01
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2018-02-19 17:00:19 +0000

    velocity and angular velocity tracking

--- Assets/Scripts/LocalTransformDataTracker.cs
+++ Assets/Scripts/LocalTransformDataTracker.cs
@@ -9,6 +9,10 @@ public class LocalTransformDataTracker : MonoBehaviour
     Transform rightHand;
     [SerializeField]
     Transform leftHand;
+	[SerializeField]
+	Rigidbody leftRB;
+	[SerializeField]
+	Rigidbody rightRB;
     [SerializeField]
     Transform head;
 
@@ -38,8 +42,10 @@ public class LocalTransformDataTracker : MonoBehaviour
         if (shooter.started)
         {
             data = new TransformData(head.localPosition.x, head.localPosition.y, head.localPosition.z, head.localRotation.x, head.localRotation.y, head.localRotation.z, head.localRotation.w,
-                                    rightHand.localPosition.x, rightHand.localPosition.y, rightHand.localPosition.z, rightHand.localRotation.x, rightHand.localRotation.y, rightHand.localRotation.z, rightHand.localRotation.w,
-                                    leftHand.localPosition.x, leftHand.localPosition.y, leftHand.localPosition.z, leftHand.localRotation.x, leftHand.localRotation.y, leftHand.localRotation.z, leftHand.localRotation.w,
+				rightHand.localPosition.x, rightHand.localPosition.y, rightHand.localPosition.z, rightHand.localRotation.x, rightHand.localRotation.y, rightHand.localRotation.z, rightHand.localRotation.w,
+				rightRB.angularVelocity.x, rightRB.angularVelocity.y, rightRB.angularVelocity.z, rightRB.velocity.x, rightRB.velocity.y, rightRB.velocity.z,
+				leftHand.localPosition.x, leftHand.localPosition.y, leftHand.localPosition.z, leftHand.localRotation.x, leftHand.localRotation.y, leftHand.localRotation.z, leftHand.localRotation.w, 
+				leftRB.angularVelocity.x, leftRB.angularVelocity.y, leftRB.angularVelocity.z, leftRB.velocity.x, leftRB.velocity.y, leftRB.velocity.z,
                                     shooter.Gesturing, shooter.GestureTime, shooter.gestureNumber);
             csvWriter.WriteRecord(data);
             csvWriter.NextRecord();
--- Assets/Scripts/TransformDataRecorder.cs
+++ Assets/Scripts/TransformDataRecorder.cs
@@ -9,6 +9,10 @@ public class TransformDataRecorder : MonoBehaviour
     Transform rightHand;
     [SerializeField]
     Transform leftHand;
+	[SerializeField]
+	Rigidbody leftRB;
+	[SerializeField]
+	Rigidbody rightRB;
     [SerializeField]
     Transform head;
 
@@ -38,8 +42,10 @@ public class TransformDataRecorder : MonoBehaviour
         if (shooter.started)
         {
             data = new TransformData(head.position.x, head.position.y, head.position.z, head.rotation.x, head.rotation.y, head.rotation.z, head.rotation.w,
-                                    rightHand.position.x, rightHand.position.y, rightHand.position.z, rightHand.rotation.x, rightHand.rotation.y, rightHand.rotation.z, rightHand.rotation.w,
-                                    leftHand.position.x, leftHand.position.y, leftHand.position.z, leftHand.rotation.x, leftHand.rotation.y, leftHand.rotation.z, leftHand.rotation.w,
+				rightHand.position.x, rightHand.position.y, rightHand.position.z, rightHand.rotation.x, rightHand.rotation.y, rightHand.rotation.z, rightHand.rotation.w,
+				rightRB.angularVelocity.x, rightRB.angularVelocity.y, rightRB.angularVelocity.z, rightRB.velocity.x, rightRB.velocity.y, rightRB.velocity.z,
+				leftHand.position.x, leftHand.position.y, leftHand.position.z, leftHand.rotation.x, leftHand.rotation.y, leftHand.rotation.z, leftHand.rotation.w,
+				leftRB.angularVelocity.x, leftRB.angularVelocity.y, leftRB.angularVelocity.z, leftRB.velocity.x, leftRB.velocity.y, leftRB.velocity.z,
                                     shooter.Gesturing, shooter.GestureTime, shooter.gestureNumber);
             csvWriter.WriteRecord(data);
             csvWriter.NextRecord();
@@ -63,6 +69,12 @@ public class TransformData
     public float RightRotY { get; set; }
     public float RightRotZ { get; set; }
     public float RightRotW { get; set; }
+	public float RightAngluarVelocityX { get; set; }
+	public float RightAngluarVelocityY { get; set; }
+	public float RightAngluarVelocityZ { get; set; }
+	public float RightVelocityX { get; set; }
+	public float RightVelocityY { get; set; }
+	public float RightVelocityZ { get; set; }
     public float LeftX { get; set; }
     public float LeftY { get; set; }
     public float LeftZ { get; set; }
@@ -70,13 +82,19 @@ public class TransformData
     public float LeftRotY { get; set; }
     public float LeftRotZ { get; set; }
     public float LeftRotW { get; set; }
+	public float LeftAngluarVelocityX { get; set; }
+	public float LeftAngluarVelocityY { get; set; }
+	public float LeftAngluarVelocityZ { get; set; }
+	public float LeftVelocityX { get; set; }
+	public float LeftVelocityY { get; set; }
+	public float LeftVelocityZ { get; set; }
     public bool Gesturing { get; set; }
     public bool GestureTime { get; set; }
     public int GestureNumber { get; set; }
 
     public TransformData(float hx, float hy, float hz, float hrx, float hry, float hrz, float hrw, 
-                        float rx, float ry, float rz, float rrx, float rry, float rrz, float rrw, 
-                        float lx, float ly, float lz, float llx, float lly, float llz, float llw, 
+		float rx, float ry, float rz, float rrx, float rry, float rrz, float rrw, float ravx, float ravy, float ravz, float rvx, float rvy, float rvz,
+		float lx, float ly, float lz, float llx, float lly, float llz, float llw, float lavx, float lavy, float lavz, float lvx, float lvy, float lvz,
                         bool gesture, bool gestureTime, int gestureNumber)
     {
         HeadX = hx;
@@ -93,13 +111,25 @@ public class TransformData
         RightRotY = rry;
         RightRotZ = rrz;
         RightRotW = rrw;
-        LeftX = lx;
+		RightAngluarVelocityX = ravx;
+		RightAngluarVelocityY = ravy;
+		RightAngluarVelocityZ = ravz;
+		RightVelocityX = rvx;
+		RightVelocityY = rvy;
+		RightVelocityZ = rvz;
+		LeftX = lx;
         LeftY = ly;
         LeftZ = lz;
         LeftRotX = llx;
         LeftRotY = lly;
         LeftRotZ = llz;
         LeftRotW = llw;
+		LeftAngluarVelocityX = lavx;
+		LeftAngluarVelocityY = lavy;
+		LeftAngluarVelocityZ = lavz;
+		LeftVelocityX = lvx;
+		LeftVelocityY = lvy;
+		LeftVelocityZ = lvz;
         Gesturing = gesture;
         GestureTime = gestureTime;
         GestureNumber = gestureNumber;
@@ -124,15 +154,27 @@ public sealed class TransformDataMap : ClassMap<TransformData>
         Map(m => m.RightRotY).Index(11);
         Map(m => m.RightRotZ).Index(12);
         Map(m => m.RightRotW).Index(13);
-        Map(m => m.LeftX).Index(14);
-        Map(m => m.LeftY).Index(15);
-        Map(m => m.LeftZ).Index(16);
-        Map(m => m.LeftRotX).Index(17);
-        Map(m => m.LeftRotY).Index(18);
-        Map(m => m.LeftRotZ).Index(19);
-        Map(m => m.LeftRotW).Index(20);
-        Map(m => m.Gesturing).Index(21);
-        Map(m => m.GestureTime).Index(22);
-        Map(m => m.GestureNumber).Index(23);
+		Map (m => m.RightAngluarVelocityX).Index (14);
+		Map (m => m.RightAngluarVelocityY).Index (15);
+		Map (m => m.RightAngluarVelocityZ).Index (16);
+		Map (m => m.RightVelocityX).Index (17);
+		Map (m => m.RightVelocityY).Index (18);
+		Map (m => m.RightVelocityZ).Index (19);
+        Map(m => m.LeftX).Index(20);
+        Map(m => m.LeftY).Index(21);
+        Map(m => m.LeftZ).Index(22);
+        Map(m => m.LeftRotX).Index(23);
+        Map(m => m.LeftRotY).Index(24);
+        Map(m => m.LeftRotZ).Index(25);
+        Map(m => m.LeftRotW).Index(26);
+		Map(m => m.LeftAngluarVelocityX).Index(27);
+		Map(m => m.LeftAngluarVelocityY).Index(28);
+		Map(m => m.LeftAngluarVelocityZ).Index(29);
+		Map(m => m.LeftVelocityX).Index(30);
+		Map(m => m.LeftVelocityY).Index(31);
+		Map(m => m.LeftVelocityZ).Index(32);
+		Map(m => m.Gesturing).Index(33);
+        Map(m => m.GestureTime).Index(34);
+        Map(m => m.GestureNumber).Index(35);
     }
 }
